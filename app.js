1e8f2d3ee653ef8099e8c0df334ebcef086a7f48
Faraz Faheem <faraz.faheem@hotmail.com>
2016-09-30 01:01:03 +0400

    setup files

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/', function (request, response) {
+    response.send('hello node');
+});
+
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
